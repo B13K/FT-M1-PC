b2adb26d4ad3053905e26d1cfe65c55f5bc7e578
Jonathan <jonathan_3_7@hotmail.com>
2022-09-21 10:06:20 -0500

    Primer Homework de IntroToCS

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,11 +2,29 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let decimal = 0
+  for(let i=0; i<num.length; i++){
+    decimal = decimal + Math.pow(2,num.length-1-i)*num[i]
+  }
+  return decimal
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let residuo = num;
+  let binario =''
+
+  while(residuo>1){
+    binario += residuo%2;
+    residuo = Math.floor(residuo/2)
+  }
+  if(residuo==1){
+    binario += 1
+  }
+  
+  let bin = binario.split('').reverse().join('')
+  return bin;
 
 }
 
