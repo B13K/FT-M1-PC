4472f84a2e30d25607e6aadaa6f07115abbb96e0
Jonathan <jonathan_3_7@hotmail.com>
2022-10-19 12:59:46 -0500

    HW 07-Algoritmos I

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,6 +6,20 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  let factores = [1];
+  let i = 0
+  let primos = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+  
+  while(num>1){
+    let mod = num%primos[i]
+    if(mod === 0){
+      factores.push(primos[i])
+      num = num/primos[i]
+    } else {
+      i++
+    }
+  }
+  return factores;
 
 }
 
@@ -14,6 +28,21 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  let swap = true
+  while(swap){
+    swap = false
+    for(let i = 0; i < array.length - 1; i++){
+      let a = array[i];
+      let b = array[i + 1]
+
+      if(b < a){
+        swap = true
+        array[i] = b
+        array[i + 1] = a
+      }
+    }
+  }
+  return array
 
 }
 
@@ -24,6 +53,27 @@ function insertionSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  for( let i = 1; i < array.length; i++){ 
+    let current = array[i];
+    let swapB = true
+    let j = i-1
+    while(swapB && j>=0){
+      swapB = false
+      let b = array[j]
+      if(current<b){
+        array[j+1] = b
+        array[j] = current
+        swapB = true
+        j--
+      } else{
+        array[j+1] = current
+      }
+      if(j+1 === i) swapB=false
+    }
+  }
+
+return array
+
 }
 
 
@@ -32,6 +82,22 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for(let i = 0; i < array.length-1; i++){
+    let current = array[i];
+    let index = i
+    let min = array[i]    
+    for(let j = i+1; j < array.length; j++){
+      let b = array[j]
+      if(b < min){
+        min = b;
+        index = j
+      }      
+    }
+    array[i] = min
+    array[index] = current
+    
+  }
+  return array
 
 }
 
